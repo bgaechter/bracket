cce3256bd6523e9c980e124ee3895c4a235adfb2
Benny Gaechter <benny.gaechter@gmail.com>
2024-02-22 15:21:54 +0100

    Remove unused auth code

--- cmd/bracket.go
+++ cmd/bracket.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"strings"
-
 	"github.com/bgaechter/bracket/internal/bracket"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/memstore"
 	"github.com/gin-gonic/gin"
 )
 
-func Auth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		principalName := c.Request.Header.Get("X-MS-CLIENT-PRINCIPAL-NAME")
-		c.Next()
-		log.Println(principalName)
-		authorizedUsers := os.Getenv("BRACKET_USERS")
-		if strings.Contains(authorizedUsers, principalName) {
-			c.Set("admin", "true")
-		} else {
-			c.Redirect(http.StatusTemporaryRedirect, "/unauthorized")
-		}
-	}
-}
-
 func main() {
 
 	router := gin.Default()
 	store := memstore.NewStore([]byte("ranker"))
 	router.Use(sessions.Sessions("session", store))
-	router.Use(Auth())
 
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", bracket.GetIndex)
